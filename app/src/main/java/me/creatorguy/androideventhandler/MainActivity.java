49ec7b1d6cb43cda1d5af4e768675ac3cb41cc2c
Smit Desai <smit.desai16@gmail.com>
2020-06-20 12:56:49 -0500

    Adding Button click handlers

--- app/src/main/java/me/creatorguy/androideventhandler/MainActivity.java
+++ app/src/main/java/me/creatorguy/androideventhandler/MainActivity.java
@@ -3,12 +3,45 @@ package me.creatorguy.androideventhandler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static Button btnMain2, btnMain3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMain2 = findViewById(R.id.btnMain2);
+        btnMain3 = findViewById(R.id.btnMain3);
+
+        btnMain2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Button 2 Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnMain3.setOnClickListener(this);
+    }
+
+    public void btnMain1Click(View view) {
+        Toast.makeText(this, "Button 1 Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnMain3:
+                Toast.makeText(this, "Button 3 Clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
     }
 }
